863e8878117644a5a98c5cee83688feebf270978
Olushola Dairo <dairostephen@outlook.com>
2024-03-15 07:09:49 -0700

    To kick start, my database administrator journey, i had to improvise

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,5 +1,4 @@
 -- Description
 /* To create a database for mySQL server thag starts newly */
--- Script to create a new database for managing customer data
-
-CREATE DATABASE hbtn_0c_0;
+-- Script to create a new database for managing customer data */
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
